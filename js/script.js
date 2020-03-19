c145a13a99c69e0bfa7a96d17d1c5dd3d6a980ec
Oriana Reina <oriana.nohemi22@gmail.com>
2020-03-19 17:12:37 -0500

    Cambiar estilos de la seccion de inicio

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-
-$(document).ready(function () {
-  $(window).scroll(function () {
+$(document).ready(function() {
+  $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     if (scroll > 400) {
       $(".navbar").css("background", "black");
+      $(".dropdown-menu").css("background", "black");
+    } else {
+      $(".navbar").css("background", "#36536e");
+      $(".dropdown-menu").css("background", "#36536e");
     }
-    else {
-      $(".navbar").css("background", "#57ABB3");
-    }
-  })
-})
+  });
+});
